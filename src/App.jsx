98359444813f10edcc6b1785f7ef48a202c21967
Gonzalo Aruquipa <gonzao777info@gmail.com>
2023-05-12 19:54:20 -0400

    header component and user

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+// import React from "react";
+import { BrowserRouter as Router} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
+import  {Header} from "./components/Header";
+import { User } from "./components/User";
 
-function App() {
+export default function App() {
 
   return (
     <>
-     <h1>hola</h1>
-    </>
-  )
+     <Router>
+    <Header />
+      <Routes>
+        <Route exact path="/users" element={<User/>}/>
+      </Routes>
+
+     </Router>
+     </>
+    
+  );
 }
 
-export default App
+ 
