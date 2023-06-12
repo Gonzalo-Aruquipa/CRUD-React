a7c0f4ce6fddf4d7f31c946d964c82dcd9ab4d8e
Gonzalo Aruquipa <gonzao777info@gmail.com>
2023-06-12 19:49:12 -0400

    update product

--- src/components/UpdateProduct.jsx
+++ src/components/UpdateProduct.jsx
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export const UpdateProduct = () => {
   const URL = "http://localhost:3000";
 
   const [product, setProduct] = useState([]);
+  const navigate = useNavigate();
 
-  console.log(product)
 
   const { id } = useParams();
 
@@ -19,39 +20,107 @@ export const UpdateProduct = () => {
   useEffect(() => {
     getProduct();
   }, [] );
+
+  const [error, setError] = useState({});
+  const [archivo, setArchivo] = useState("");
+
+  let handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+    setError(validate({ ...product, [e.target.name]: e.target.value }));
+  };
+
+  let handleImage = (e) => {
+    setArchivo(e.target.files[0])
+  }
+
+
+  const updateProduct = async () => {
+
+    const formData = new FormData();
+    formData.append("name", product.name)
+    formData.append("price", product.price)
+    formData.append("image", archivo);
+    try {
+      await axios.put(`${URL}/products/${id}`, formData);
+      Swal.fire("OK", "El producto se editó correctamente", "success");
+        navigate("/productos")
+    } catch (error) {
+        console.log(error)
+      Swal.fire("Hubo un error", "Vuelva a Intentarlo", "error");
+        navigate("/productos")
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateProduct();
+
+    setProduct({
+      name: "",
+      price: "",
+    });
+  };
+
+  function validate(product) {
+    const error = {};
+    if (!product.name) {
+      error.name = "El Nombre no puede ir vacío";
+    } 
+    if (!product.price) {
+      error.price = "El precio no puede ir vacío";
+    } 
+    return error;
+  }
   return (
     <>
       <h2>Editar Producto</h2>
 
-      <form action="/productos" method="POST">
+      <form onSubmit={handleSubmit}>
         <legend>Llena todos los campos</legend>
 
         <div className="campo">
           <label>Nombre:</label>
-          <input type="text" placeholder="Nombre Producto" name="nombre" />
+          <input
+            type="text"
+            placeholder="Nombre Producto"
+            name="name"
+            value={product.name}
+            onChange={handleChange}
+          />
+          {error.name && <p className="danger-p">{error.name}</p>}
         </div>
 
         <div className="campo">
           <label>Precio:</label>
           <input
             type="number"
-            name="precio"
+            name="price"
             min="0.00"
-            step="0.01"
+            step="1"
             placeholder="Precio"
+            value={product.price}
+            onChange={handleChange}
           />
+          {error.price && <p className="danger-p">{error.price}</p>}
         </div>
 
         <div className="campo">
           <label>Imagen:</label>
-          <input type="file" name="imagen" />
+          <input 
+          type="file" 
+          name="image"
+          onChange={handleImage}
+           />
         </div>
 
         <div className="enviar">
           <input
             type="submit"
             className="btn btn-azul"
-            value="Agregar Producto"
+            value="Guardar Cambios"
+            disabled={error.name || error.price 
+              ? true
+              : false}
           />
         </div>
       </form>
